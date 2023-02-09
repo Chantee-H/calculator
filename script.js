4edcb5e494fcd40fac58b8eac9284568aa07d5c1
Chantee <Chantee.hoarau123@gmail.com>
2023-02-09 10:47:48 +0200

    Create a function that takes 2 operands and an operator

---
+++ script.js
@@ -0,0 +1,30 @@
+const numbers = document.querySelectorAll('.digit');
+const operators = document.querySelectorAll('.operators');
+const display1 = document.querySelector('.display1');
+const display2 = document.querySelector('.display2');
+let firstOperand = '';
+let secondOperand = '';
+let operator = '';
+
+function operate(){
+    const first = parseFloat(firstOperand);
+    const second = parseFloat(secondOperand);
+    let result;
+    switch(operator) {
+        case '+':
+            result = first + second
+            break;
+        case '-':
+            result = first - second
+            break;
+        case 'x':
+            result = first * second
+            break;
+        case '/':
+            result = first / second
+            break;
+    }
+
+    secondOperand = result;
+    firstOperand = '';
+}
